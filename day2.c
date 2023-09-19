f1b43cb2b4250b70bc205da4dfc713419f9a4669
shivamjha2054 <145041172+shivamjha2054@users.noreply.github.com>
2023-09-19 19:17:11 +0530

    Add files via upload

---
+++ day2.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+
+int main(){
+   int a,b;
+   printf("enter a");
+   scanf("%d",&a);
+
+   printf("enter b");
+   scanf("%d",&b);
+
+   int multiply=a*b;
+   printf("%d",multiply);
+   return 0;
+   
+}
