ce86c3d90f7b8d48b711695243af41e4dcc1e092
shivamjha2054 <145041172+shivamjha2054@users.noreply.github.com>
2023-10-27 20:47:14 +0530

    Add files via upload

---
+++ day40.c
@@ -0,0 +1,46 @@
+#include<stdio.h>
+int main(){
+    int m;
+    printf("Enter m(1-12):");
+    scanf("%d",&m);
+
+    if(m==1){
+        printf("Days = 31\n");
+    }
+    else if(m==3){
+        printf("Days = 31\n");
+    }
+    else if(m==5){
+        printf("Days = 31\n");
+    }
+    else if(m==7){
+        printf("Days = 31\n");
+    }
+    else if(m==8){
+        printf("Days = 31\n");
+    }
+    else if(m==10){
+        printf("Days = 31\n");
+    }
+    else if(m==12){
+        printf("Days = 31\n");
+    }
+    else if(m==2){
+        printf("Days = 28\n");
+    }
+    else if(m==4){
+        printf("Days = 30\n");
+    }
+    else if(m==6){
+        printf("Days = 30\n");
+    }
+    else if(m==9){
+        printf("Days = 30\n");
+    }
+    else if(m==11){
+        printf("Days = 30\n");
+    }
+    else{
+        printf("Sorry for the inconvinience caused\n");
+    }
+}
